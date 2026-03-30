b2d322a11c4c21af0844521579ac7e346ef51a66
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-03-30 14:15:38 +0545

    add exception for email check

---
+++ src/main/java/com/doctorappointment/patient_service/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.doctorappointment.patient_service.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
