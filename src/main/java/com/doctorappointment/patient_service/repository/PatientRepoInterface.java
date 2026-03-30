e36f9809c6f1af24e9cf1bc4422d3b71e93d02c8
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-03-30 20:33:27 +0545

    add a email repo to check duplicate email

--- src/main/java/com/doctorappointment/patient_service/repository/PatientRepoInterface.java
+++ src/main/java/com/doctorappointment/patient_service/repository/PatientRepoInterface.java
@@ -7,9 +7,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface PatientRepoInterface {
-    void addPatient(PatientModel patient);
+    PatientModel addPatient(PatientModel patient);
    Optional<PatientModel> getPatientById(UUID id);
     Optional<PatientModel> getPatientByEmail(String email);
     void updatePatient(PatientModel patient);
     void deletePatient(UUID id);
+    boolean existsPatientByEmail(String email);
 }
