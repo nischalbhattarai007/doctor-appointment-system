d6ad7735047f64ec25a8ff8c0dc69b36a49aaa55
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-03-31 09:39:35 +0545

    code cleanup and optimize

--- src/main/java/com/doctorappointment/patient_service/dto/PatientModel.java
+++ src/main/java/com/doctorappointment/patient_service/dto/PatientModel.java
@@ -13,66 +13,66 @@ import java.util.regex.Pattern;
 @Introspected
 @Serdeable
 @Builder(toBuilder = true)
-public record PatientModel (UUID patientId,
-                            String firstName,
-                            String lastName,
-                            String email,
-                            String password,
-                            boolean isDeleted,
-                            String phoneNumber,
-                            String address) {
+public record PatientModel(UUID patientId,
+                           String firstName,
+                           String lastName,
+                           String email,
+                           String password,
+                           boolean isDeleted,
+                           String phoneNumber,
+                           String address) {
 
     public PatientModel {
         //name validation
-            if (isNullOrEmpty(firstName) ){
-                throw new ValidationException(" first name cannot be empty or null");
-            }
-            if (isNullOrEmpty(lastName) ){
-                throw new ValidationException(" last name cannot be empty or null");
-            }
+        if (isNullOrEmpty(firstName)) {
+            throw new ValidationException(" first name cannot be empty or null");
+        }
+        if (isNullOrEmpty(lastName)) {
+            throw new ValidationException(" last name cannot be empty or null");
+        }
 
-            //email validation
-        if(isNullOrEmpty(email) ){
+        //email validation
+        if (isNullOrEmpty(email)) {
             throw new ValidationException(" email cannot be empty or null");
         }
-            String emailRegex= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-            if(!Pattern.matches(emailRegex,email)){
-                throw new InvalidEmailException("Invalid email address");
-            }
+        String emailRegex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        if (!Pattern.matches(emailRegex, email)) {
+            throw new InvalidEmailException("Invalid email address");
+        }
 
-            //phone validation
-            if(isNullOrEmpty(phoneNumber) ){
-                throw new ValidationException(" phone number cannot be empty or null");
-            }
-            String normalizedPhone=phoneNumber;
-            if(phoneNumber.startsWith("+977")){
-                normalizedPhone=phoneNumber.substring(4);
-            }else if(phoneNumber.startsWith("977")){
-                normalizedPhone=phoneNumber.substring(3);
-            }
-            if(!normalizedPhone.matches("\\d{10}")){
-                throw new InvalidPhoneNumberException("Invalid phone number");
-            }
-            if(!normalizedPhone.startsWith("97") && !normalizedPhone.startsWith("98")){
-                throw new InvalidPhoneNumberException("Invalid phone number");
-            }
-            phoneNumber="+977 " + normalizedPhone;
+        //phone validation
+        if (isNullOrEmpty(phoneNumber)) {
+            throw new ValidationException(" phone number cannot be empty or null");
+        }
+        String normalizedPhone = phoneNumber;
+        if (phoneNumber.startsWith("+977")) {
+            normalizedPhone = phoneNumber.substring(4);
+        } else if (phoneNumber.startsWith("977")) {
+            normalizedPhone = phoneNumber.substring(3);
+        }
+        if (!normalizedPhone.matches("\\d{10}")) {
+            throw new InvalidPhoneNumberException("Invalid phone number");
+        }
+        if (!normalizedPhone.startsWith("97") && !normalizedPhone.startsWith("98")) {
+            throw new InvalidPhoneNumberException("Invalid phone number");
+        }
+        phoneNumber = "+977 " + normalizedPhone;
 
-            //password validation
-        if(isNullOrEmpty(password)){
+        //password validation
+        if (isNullOrEmpty(password)) {
             throw new ValidationException(" password cannot be empty or null");
         }
-        if(password.length() < 8){
+        if (password.length() < 8) {
             throw new ValidationException(" password must be at least 8 characters long");
         }
 
         //address validation
-        if(isNullOrEmpty(address)){
+        if (isNullOrEmpty(address)) {
             throw new ValidationException(" address cannot be empty or null");
         }
     }
 
-    public boolean isNullOrEmpty(String value){
+    public boolean isNullOrEmpty(String value) {
         return value == null || value.isEmpty();
     }
 
