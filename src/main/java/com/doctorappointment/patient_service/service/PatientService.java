f9e7f0fe024cd5ce7006dd2901124fe252029c2e
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-03-31 11:55:00 +0545

    add a bcrypt to hash password

--- src/main/java/com/doctorappointment/patient_service/service/PatientService.java
+++ src/main/java/com/doctorappointment/patient_service/service/PatientService.java
@@ -3,6 +3,7 @@ import com.doctorappointment.patient_service.dto.PatientModel;
 import com.doctorappointment.patient_service.exception.EmailAlreadyExistsException;
 import com.doctorappointment.patient_service.repository.PatientRepoInterface;
 import jakarta.inject.Singleton;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.UUID;
 
@@ -18,9 +19,10 @@ public class PatientService {
         if(patientRepo.existsPatientByEmail(patient.email())) {
             throw new EmailAlreadyExistsException("Email already exists");
         }
-
+        String hashedPassword= BCrypt.hashpw(patient.password(), BCrypt.gensalt());
         return patientRepo.addPatient(patient.toBuilder()
                 .patientId(UUID.randomUUID())
+                .password(hashedPassword)
                 .build());
     }
 }
