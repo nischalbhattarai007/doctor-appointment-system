2d318baae2d9b89d08e305963a160dae37bd1e3f
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-04-02 11:15:59 +0545

    add a doctor query and schema

---
+++ src/main/java/com/doctorappointment/doctor_service/constant/DoctorQuery.java
@@ -0,0 +1,60 @@
+package com.doctorappointment.doctor_service.constant;
+
+import static com.doctorappointment.doctor_service.constant.DoctorSchema.*;
+
+public class DoctorQuery {
+
+    //insert into doctor query
+    public static final String INSERT =
+            "INSERT INTO " + DOCTORS_TABLE +
+                    " (" +
+                    DOCTOR_ID + ", " +
+                    FIRST_NAME + ", " +
+                    LAST_NAME + ", " +
+                    EMAIL + ", " +
+                    PASSWORD + ", " +
+                    PHONE_NUMBER + ", " +
+                    ADDRESS + ", " +
+                    SPECIALIZATION + ", " +
+                    CLINIC_ADDRESS + ", " +
+                    LATITUDE + ", " +
+                    LONGITUDE + ", " +
+                    IS_DELETED +
+                    ") VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    //find by id
+    public static final String FIND_BY_ID =
+            "SELECT * FROM " + DOCTORS_TABLE +
+                    " WHERE " + DOCTOR_ID + " =?";
+
+    //find by email
+    public static final String FIND_BY_EMAIL =
+            "SELECT * FROM " + DOCTORS_TABLE +
+                    " WHERE " + EMAIL + " =?";
+
+    //update doctor by id
+    public static final String UPDATE =
+            "UPDATE " + DOCTORS_TABLE +
+                    " SET " +
+                    FIRST_NAME + " =?, " +
+                    LAST_NAME + " =?, " +
+                    PHONE_NUMBER + " =?, " +
+                    ADDRESS + " =?, " +
+                    SPECIALIZATION + " =?, " +
+                    CLINIC_ADDRESS + " =?, " +
+                    LATITUDE + " =?, " +
+                    LONGITUDE + " =?" +
+                    " WHERE " + DOCTOR_ID + " =?";
+
+    //soft delete doctor by id
+    public static final String SOFT_DELETE =
+            "UPDATE " + DOCTORS_TABLE +
+                    " SET " + IS_DELETED + " = true" +
+                    " WHERE " + DOCTOR_ID + " =?";
+
+    //exists by email
+    public static final String EXISTS_BY_EMAIL =
+            "SELECT " + EMAIL + " FROM " + DOCTORS_TABLE +
+                    " WHERE " + EMAIL + " =?";
+
+}
---
+++ src/main/java/com/doctorappointment/doctor_service/constant/DoctorSchema.java
@@ -0,0 +1,23 @@
+package com.doctorappointment.doctor_service.constant;
+
+public class DoctorSchema {
+    //keyspace
+    public static final String KEYSPACE = "doctor_appointment";
+
+    //merger keyspace with database table
+    public static final String DOCTORS_TABLE = KEYSPACE + ".doctors";
+
+    //column
+    public static final String DOCTOR_ID = "doctor_id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String PHONE_NUMBER = "phone_number";
+    public static final String ADDRESS = "address";
+    public static final String SPECIALIZATION = "specialization";
+    public static final String CLINIC_ADDRESS = "clinic_address";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String IS_DELETED = "is_deleted";
+}
