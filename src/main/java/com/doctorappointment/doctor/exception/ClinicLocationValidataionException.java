e8535e0bd9bc3bd2f7bc68fae2d1c36b751c8895
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-04-09 10:35:35 +0545

    custom exception to validate clinic location

---
+++ src/main/java/com/doctorappointment/doctor/exception/ClinicLocationValidataionException.java
@@ -0,0 +1,7 @@
+package com.doctorappointment.doctor.exception;
+
+public class ClinicLocationValidataionException extends RuntimeException {
+    public ClinicLocationValidataionException(String message) {
+        super(message);
+    }
+}
